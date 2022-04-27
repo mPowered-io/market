739ec2391794267accda0e3464b41e3346f6feae
Corrie Sloot <corrie@sloot.ca>
2022-04-27 15:11:34 -0400

    change terms & conditions

--- src/components/molecules/FormFields/Terms.tsx
+++ src/components/molecules/FormFields/Terms.tsx
@@ -2,29 +2,23 @@ import React, { ReactElement } from 'react'
 import { InputProps } from '../../atoms/Input'
 import InputElement from '../../atoms/Input/InputElement'
 import styles from './Terms.module.css'
-import { graphql, useStaticQuery } from 'gatsby'
-
-const query = graphql`
-  query TermsQuery {
-    terms: markdownRemark(fields: { slug: { eq: "/terms" } }) {
-      html
-    }
-  }
-`
+import { useSiteMetadata } from '../../../hooks/useSiteMetadata'
 
 export default function Terms(props: InputProps): ReactElement {
-  const data = useStaticQuery(query)
+  const { appConfig } = useSiteMetadata()
   const termsProps: InputProps = {
     ...props,
     defaultChecked: props.value.toString() === 'true'
   }
 
+  const termsURL =
+    appConfig.metadataCacheUri.replace('aquarius.', '') + '/terms'
+
   return (
     <>
-      <div
-        className={styles.terms}
-        dangerouslySetInnerHTML={{ __html: data.terms.html }}
-      />
+      <a href={termsURL} target="_blank">
+        Click here for the Terms &amp; Conditions
+      </a>
       <InputElement {...termsProps} type="checkbox" />
     </>
   )
